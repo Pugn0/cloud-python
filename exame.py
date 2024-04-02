f2cf3c4db59aaefb481e9ae3d41fdd9be2eea921
Pugno <109306901+Pugn0@users.noreply.github.com>
2024-04-02 13:59:08 -0300

    Add files via upload

--- exame.py
+++ exame.py
@@ -1,33 +1,20 @@
 import os
 
-# Definindo a pasta onde os arquivos .txt estão localizados
 pasta = 'clouds/'
-
-# Lista de protocolos para procurar
 protocolos = ['http://', 'https://', 'ftp://']
-
-# Inicializando um dicionário para contar as ocorrências de cada protocolo
 contagem_protocolos = {protocolo: 0 for protocolo in protocolos}
 
-# Percorrendo os arquivos na pasta
 for arquivo in os.listdir(pasta):
-    # Certifique-se de que estamos trabalhando apenas com arquivos .txt
     if arquivo.endswith('.txt'):
-        # Construindo o caminho completo para o arquivo
         caminho_completo = os.path.join(pasta, arquivo)
-
-        # Abrindo e lendo o arquivo linha por linha
-        with open(caminho_completo, 'r', encoding='utf-8') as f:
+        with open(caminho_completo, 'r', encoding='utf-8', errors='ignore') as f:
             for linha in f:
-                # Contando as ocorrências de cada protocolo na linha
                 for protocolo in protocolos:
                     contagem_protocolos[protocolo] += linha.count(protocolo)
 
-        # Atualizando a contagem em tempo real
         atualizacao = ' '.join([f"{proto}: {cont}" for proto, cont in contagem_protocolos.items()])
         print(f"\r{atualizacao}", end="", flush=True)
 
-# Quebra de linha final após o loop completar
 print("\n\nContagem final de protocolos encontrados:")
 for protocolo, contagem in contagem_protocolos.items():
-    print
+    print(f"{protocolo}: {contagem}")

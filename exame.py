92c0a3a43fe8a4078e93374a90dccd1d1c9cfcba
Pugno <109306901+Pugn0@users.noreply.github.com>
2024-04-02 17:36:03 -0300

    Add files via upload

--- exame.py
+++ exame.py
@@ -3,6 +3,10 @@ import os
 # Definindo a pasta onde os arquivos .txt estão localizados
 pasta = 'clouds/exame/'
 
+# Certifique-se de que a pasta /filtro/ existe ou crie-a
+filtro_pasta = os.path.join(pasta, "filtro")
+os.makedirs(filtro_pasta, exist_ok=True)
+
 # Lista expandida de protocolos para procurar
 # Padrões de Internet e Redes (URI com base IANA)
 protocolos = [
@@ -26,7 +30,8 @@ protocolos = [
         'teliaeid://' 'things://' 'thismessage://' 'tip://' 'tn3270://' 'tool://' 'turn://' 'turns://'
         'tv://' 'udp://' 'unreal://' 'urn://' 'ut2004://' 'vemmi://' 'ventrilo://' 'videotex://' 'view-source://'
         'wais://' 'webcal://' 'wpid://' 'ws://' 'wss://' 'wtai://' 'wyciwyg://' 'xcon://' 'xcon-userid://'
-        'xfire://' 'xmlrpc.beep://' 'xmlrpc.beeps://' 'xmpp://' 'xri://' 'ymsgr://' 'z39.50r://' 'z39.50s://'
+        'xfire://' 'xmlrpc.beep://' 'xmlrpc.beeps://' 'xmpp://' 'xri://' 'ymsgr://' 'z39.50r://' 'z39.50s://', 'oauth://', 'chrome-extension://', 'file:///', 'mailbox://',
+        'moz-proxy://', 'pop://', 'content://'
     ]
 # Inicializando um dicionário para contar as ocorrências de cada protocolo
 contagem_protocolos = {protocolo: 0 for protocolo in protocolos}
@@ -35,15 +40,29 @@ contagem_protocolos = {protocolo: 0 for protocolo in protocolos}
 for arquivo in os.listdir(pasta):
     if arquivo.endswith('.txt'):
         caminho_completo = os.path.join(pasta, arquivo)
-        with open(caminho_completo, 'r', encoding='utf-8', errors='ignore') as f:
-            for linha in f:
-                for protocolo in protocolos:
-                    ocorrencias = linha.count(protocolo)
-                    if ocorrencias:
-                        contagem_protocolos[protocolo] += ocorrencias
-                        # Atualizando a contagem em tempo real
-                        atualizacao = ' '.join([f"{proto}: {cont}" for proto, cont in contagem_protocolos.items() if cont > 0])
-                        print(f"\r{atualizacao}", end="", flush=True)
-                        
+        linhas_para_manter = []
 
-print("\n\nFinalizado:")
+        with open(caminho_completo, 'r', encoding='utf-8', errors='ignore') as f:
+            linhas = f.readlines()
+
+        for linha in linhas:
+            encontrou_protocolo = False
+            for protocolo in protocolos:
+                if protocolo in linha:
+                    encontrou_protocolo = True
+                    contagem_protocolos[protocolo] += linha.count(protocolo)
+                    nome_arquivo_protocolo = os.path.join(filtro_pasta, protocolo.split('://')[0] + '.txt')
+                    with open(nome_arquivo_protocolo, 'a', encoding='utf-8') as p_file:
+                        p_file.write(linha)
+                    # Atualiza a contagem em tempo real no console
+                    atualizacao = ' '.join([f"{proto.split('://')[0]}: {cont}" for proto, cont in contagem_protocolos.items() if cont > 0])
+                    print(f"\r{atualizacao}", end="", flush=True)
+                    break  # Assumindo que uma linha vai para um arquivo baseado no primeiro protocolo encontrado
+            if not encontrou_protocolo:
+                linhas_para_manter.append(linha)
+
+        # Reescrever o arquivo original sem as linhas que contêm protocolos
+        with open(caminho_completo, 'w', encoding='utf-8', errors='ignore') as f:
+            f.writelines(linhas_para_manter)
+
+print("\nProcessamento concluído.")

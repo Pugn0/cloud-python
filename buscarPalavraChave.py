ec8c4b882864cd71e5bb4c71363e361b86f9c10f
Pugno <109306901+Pugn0@users.noreply.github.com>
2024-04-17 23:19:02 -0300

    Add files via upload

--- buscarPalavraChave.py
+++ buscarPalavraChave.py
@@ -3,7 +3,11 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 import json
-from datetime import datetime
+from datetime import datetime, timezone
+from tqdm import tqdm
+from colorama import init, Fore
+
+init(autoreset=True)  # Inicializa colorama
 
 # Função para extrair detalhes da linha
 def extrair_detalhes(linha):
@@ -27,52 +31,44 @@ def normalizar_dados(dominio, usuario, senha):
     return dominio, usuario, senha
 
 # Função para criar o objeto JSON
-def criar_json(dominio, usuario, senha, linha, timestamp, source, category, email, phone, nick):
+def criar_json(dominio, usuario, senha, linha):
     objeto = {
         "url": dominio,
         "username": usuario,
         "password": senha,
-        "line": linha,
-        "collection_timestamp": timestamp,
-        "source": source,
-        "category": category,
-        "contact_email": email,
-        "contact_phone": phone,
-        "contact_nick": nick
+        "line": linha
     }
     return json.dumps(objeto, indent=2)
 
-def processar_arquivo(caminho_do_arquivo, palavra_chave, source, category, email, phone, nick):
-    timestamp = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+def processar_arquivo(caminho_do_arquivo, palavra_chave, resultados, pbar):
+    timestamp = datetime.now(timezone.utc).replace(microsecond=0).isoformat() + "Z"
     try:
+        total_resultados = 0
         with open(caminho_do_arquivo, 'r', encoding='utf-8', errors='ignore') as arquivo:
-            resultados = []
             for linha in arquivo:
                 if palavra_chave.lower() in linha.lower():
                     dominio, usuario, senha, linha = extrair_detalhes(linha.strip())
                     if dominio and usuario and senha:
-                        objeto_json = criar_json(dominio, usuario, senha, linha, timestamp, source, category, email, phone, nick)
+                        objeto_json = criar_json(dominio, usuario, senha, linha)
                         resultados.append(objeto_json)
-                        print(objeto_json)
-            if resultados:
-                salvar_resultados = input("Deseja salvar os resultados em um arquivo? (S/N): ").strip().upper()
-                if salvar_resultados == 'S':
-                    nome_arquivo = input("Digite o nome do arquivo para salvar (sem extensão): ")
-                    nome_arquivo += ".txt"
-                    local_arquivo = filedialog.asksaveasfilename(defaultextension=".txt", initialfile=nome_arquivo, title="Salvar como")
-                    with open(local_arquivo, 'w') as arquivo_saida:
-                        for resultado in resultados:
-                            arquivo_saida.write(resultado + '\n')
-                    print(f"Os resultados foram salvos em {local_arquivo}")
+                        total_resultados += 1
+                        # Atualiza a descrição da barra de progresso com a quantidade de resultados encontrados
+                        pbar.set_description(f"{Fore.BLUE}Progresso - {total_resultados} resultados encontrados")
+        pbar.update(1)  # Incrementa o progresso da barra
     except Exception as e:
-        print(f"Ocorreu um erro ao processar o arquivo: {e}")
+        print(f"{Fore.RED}Ocorreu um erro ao processar o arquivo: {e}")
 
-def processar_diretorio(diretorio, palavra_chave, source, category, email, phone, nick):
-    for arquivo in os.listdir(diretorio):
-        if arquivo.endswith(".txt"):
+
+
+def processar_diretorio(diretorio, palavra_chave):
+    resultados = []
+    arquivos = [arquivo for arquivo in os.listdir(diretorio) if arquivo.endswith(".txt")]
+    with tqdm(total=len(arquivos), desc=f"{Fore.BLUE}Progresso", unit="arquivos", bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)) as pbar:
+        for arquivo in arquivos:
             caminho_completo = os.path.join(diretorio, arquivo)
-            print(f"Processando arquivo: {caminho_completo}")
-            processar_arquivo(caminho_completo, palavra_chave, source, category, email, phone, nick)
+            processar_arquivo(caminho_completo, palavra_chave, resultados, pbar)  # Adiciona o argumento pbar
+    return resultados
+
 
 def selecionar_arquivo_ou_diretorio():
     root = tk.Tk()
@@ -91,17 +87,29 @@ def coletar_dados_e_processar():
     caminho, tipo = selecionar_arquivo_ou_diretorio()
     if caminho:
         palavra_chave = input("Digite a palavra-chave para filtrar: ")
-        source = input("Origem da Coleta: ")
-        category = input("Categoria ou Tipo: ")
-        email = input("Informações de Contato - E-mail: ")
-        phone = input("Informações de Contato - Telefone: ")
-        nick = input("Informações de Contato - @Nick: ")
         if tipo == 'a':
-            processar_arquivo(caminho, palavra_chave, source, category, email, phone, nick)
+            resultados = []
+            processar_arquivo(caminho, palavra_chave, resultados)
+            mostrar_resultados(resultados)
         elif tipo == 'd':
-            processar_diretorio(caminho, palavra_chave, source, category, email, phone, nick)
+            resultados = processar_diretorio(caminho, palavra_chave)
+            mostrar_resultados(resultados)
     else:
         print("Nenhuma seleção foi feita.")
+
+def mostrar_resultados(resultados):
+    if resultados:
+        salvar_resultados = input("Deseja salvar os resultados em um arquivo? (S/N): ").strip().upper()
+        if salvar_resultados == 'S':
+            nome_arquivo = input("Digite o nome do arquivo para salvar (sem extensão): ")
+            nome_arquivo += ".txt"
+            local_arquivo = filedialog.asksaveasfilename(defaultextension=".txt", initialfile=nome_arquivo, title="Salvar como")
+            with open(local_arquivo, 'w') as arquivo_saida:
+                for resultado in resultados:
+                    arquivo_saida.write(resultado + '\n')
+            print(f"Os resultados foram salvos em {local_arquivo}")
+    else:
+        print(f"{Fore.YELLOW}Nenhum resultado encontrado.")
 
 if __name__ == "__main__":
     coletar_dados_e_processar()
